75cf96eca16618f5820649541578e189de40d5c3
Lapov92 <vincentlaponte@mail.adelphi.edu>
2017-02-22 02:17:30 -0500

    Add files via upload

---
+++ Lapov92FixedMenu.py
@@ -0,0 +1,37 @@
+print('\n'*100)
+
+n = 10
+test = True
+
+while test: 
+    # Menu
+    print('a. Say Hello!')
+    print('b. Count to '+str(n))
+    print('c. Say Goodbye.')
+    print(' ')
+    print('Enter a choice> ')
+    choice = input()
+
+    if choice == 'a':
+        print('Hello world!')
+        print(' ')
+        print('Press enter to continue')
+        choice = input()
+
+    if choice == 'b':
+        print(' ')
+        i = 0
+        while i<=n:
+            print(i)
+            i = i+1
+        print ('Press enter to continue')
+        choice = input()
+
+    if choice == 'c':
+        print('Goodbye!')
+        test = False
+
+
+
+
+    
